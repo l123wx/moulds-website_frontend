63e98323a3127f7de3035161b975362605082503
Elvis Liao <llwxi@qq.com>
2025-08-12 17:07:01 +0800

    fix: 修复 banner 自动高度问题

--- components/Index/Banner.vue
+++ components/Index/Banner.vue
@@ -1,25 +1,32 @@
 <template>
     <div class="banner">
         <div v-if="error">{{ $t('http.error') }}</div>
-        <el-carousel v-else height="auto" :interval="3000">
+        <el-carousel v-else ref="carouselRef" height="auto" :interval="3000" trigger="click">
             <el-carousel-item v-for="(banner, index) in bannerList" :key="index">
                 <Link v-if="banner.link" :to="banner.link" :link-type="banner.linkType" :open-type="banner.openType" class="banner-link">
-                    <NuxtImg loading="lazy" :src="banner.imagePath" :alt="$t('Banner Image')" class="banner-image" />
+                    <NuxtImg loading="lazy" :src="banner.imagePath" :alt="$t('Banner Image')" class="banner-image" @load="handleImageLoad" />
                 </Link>
-                <NuxtImg v-else loading="lazy" :src="banner.imagePath" :alt="$t('Banner Image')" class="banner-image" />
+                <NuxtImg v-else loading="lazy" :src="banner.imagePath" :alt="$t('Banner Image')" class="banner-image" @load="handleImageLoad" />
             </el-carousel-item>
         </el-carousel>
     </div>
 </template>
 
 <script setup lang="ts">
+    import type { ElCarousel } from 'element-plus'
     import getBanners from '~/http/apis/getBanners'
     import Link from '~/components/Link.vue'
+
+    const carouselRef = ref<InstanceType<typeof ElCarousel>>()
 
     const { data: bannerList, error } = useAsyncData(() => getBanners(), {
         transform: data => data?.data,
         default: () => []
     })
+
+    const handleImageLoad = () => {
+        carouselRef.value?.$forceUpdate()
+    }
 </script>
 
 <style scoped lang="less">
@@ -30,9 +37,6 @@
 
 .banner-image {
     width: 100%;
-    // min-height: 100px;
-    // 临时解决banner高度问题
-    min-height: 438px;
     object-fit: cover;
     display: block;
     margin: 0 auto;
@@ -45,11 +49,12 @@
 
 :deep(.el-carousel__indicators) {
     z-index: 10;
+    bottom: -14px;
 }
 
 :deep(.el-carousel__button) {
-    height: 12px;
-    width: 12px;
+    height: 10px;
+    width: 10px;
     background-color: #999;
     border-radius: 50%;
 }

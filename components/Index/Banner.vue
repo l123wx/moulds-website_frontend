e7d40d61cbf8a6faa504ee88272a0306d95a34b4
Elvis Liao <llwxi@qq.com>
2025-09-03 19:59:37 +0800

    feat(index/Banner): 提高banner图片加载的优先级

--- components/Index/Banner.vue
+++ components/Index/Banner.vue
@@ -2,22 +2,25 @@
     <div class="banner">
         <div v-if="error">{{ $t('http.error') }}</div>
         <UCarousel
-            v-slot="{ item: banner, index }"
-            arrows
-            dots
-            :items="bannerList"
-            :ui="{
+            v-slot="{ item: banner, index }" arrows dots :items="bannerList" :ui="{
                 dots: 'bottom-0',
                 dot: 'w-[10px] h-[10px] !bg-[#999] opacity-20 data-[state=active]:opacity-100',
                 arrows: 'absolute top-[50%] translate-y-[-50%] z-10 w-full',
                 prev: '!start-4',
                 next: '!end-4'
-            }"
-        >
-            <Link v-if="banner.link" :to="banner.link" :link-type="banner.linkType" :open-type="banner.openType" class="banner-link">
-                <NuxtImg loading="lazy" :placeholder="[300, 100, 100]" :preload="index === 0" format="webp" :src="banner.imagePath" :alt="$t('Banner Image')" class="banner-image" @load="handleImageLoad" />
+            }">
+            <Link
+                v-if="banner.link" :to="banner.link" :link-type="banner.linkType" :open-type="banner.openType"
+                class="banner-link">
+                <NuxtImg
+                    loading="lazy" fetch-priority="high" :placeholder="[300, 100, 100]" :preload="index === 0"
+                    format="webp" :src="banner.imagePath" :alt="$t('Banner Image')" class="banner-image"
+                    @load="handleImageLoad" />
             </Link>
-            <NuxtImg v-else loading="lazy" :placeholder="[300, 100, 100]" :preload="index === 0" format="webp" :src="banner.imagePath" :alt="$t('Banner Image')" class="banner-image" @load="handleImageLoad" />
+            <NuxtImg
+                v-else loading="lazy" fetch-priority="high" :placeholder="[300, 100, 100]" :preload="index === 0"
+                format="webp" :src="banner.imagePath" :alt="$t('Banner Image')" class="banner-image"
+                @load="handleImageLoad" />
         </UCarousel>
     </div>
 </template>
